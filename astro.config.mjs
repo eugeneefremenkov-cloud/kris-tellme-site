775401efc2fe2b2d69db94d9cc4ff8530805f198
Eugene Efremenkov <eugene.efremenkov@gmail.com>
2026-05-15 22:21:05 +0300

    chore: set site URL to real Vercel domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://kris-tellme.vercel.app',
+  site: 'https://kris-tellme-site.vercel.app',
   vite: {
     plugins: [tailwindcss()],
     server: {
